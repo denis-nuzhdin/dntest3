e6303cb3d057e6c83907940d16ff3b740fc873a8
denis-nuzhdin <kashmir@list.ru>
2017-05-10 22:25:24 +0400

    refactoring

--- test/open_page.py
+++ test/open_page.py
@@ -7,38 +7,37 @@ import time
 success = True
 wd = WebDriver()
 wd.implicitly_wait(60)
+main_page = "http://demo07.i-sys.ru:3003/"
+username = "i-sys\dt-tst-1"
+password = "PassW0rd"
 
-def is_alert_present(wd):
-    try:
-        wd.switch_to_alert().text
-        return True
-    except:
-        return False
+def open_start_page():
+    wd.get(main_page)
+    wd.find_element_by_id("username").click()
+    wd.find_element_by_id("username").send_keys(username)
+    wd.find_element_by_id("password").click()
+    wd.find_element_by_id("password").send_keys(password)
+    wd.find_element_by_id("SubmitCreds").click()
+
 
 try:
-    wd.get("http://demo07.i-sys.ru:3003/")
-    wd.find_element_by_id("username").click()
-    wd.find_element_by_id("username").send_keys("i-sys\dt-tst-1")
-    wd.find_element_by_id("password").click()
-    wd.find_element_by_id("password").send_keys("PassW0rd")
-    wd.find_element_by_id("SubmitCreds").click()
+    open_start_page()
     wd.find_element_by_xpath(".//li[contains(*,'Журналы регистрации')]//a[contains(span,'Входящие')]").click()
-    #$x(".//li[contains(*,'Журналы регистрации')]//a[contains(span,'Входящие')]")
     wd.find_element_by_xpath(".//*[@id='idHomePageNewItem']/span[text()='Создайте элемент']").click()
     #переключение в iframe
     frame = wd.find_element_by_xpath(".//iframe[contains(@id,'DlgFrame')]")
     wd.switch_to.frame(frame)
+
     #Исходящий номер
     ReferenceNumber = wd.find_element_by_xpath(".//input[contains(@id,'ReferenceNumberEditContainerBody')]")
     ReferenceNumber.send_keys("123")
+
 
     #поиск инпута по названию поля
     #$x(".//nobr[contains(.,'Исходящий номер')]//ancestor::tr[1]//input")
 
 
-    #wd.switch_to.frame(wd.find_element_by_xpath(".//input[contains(@id,'ReferenceNumberEditContainerBody')]"))
-    #element = wd.find_element_by_xpath(".//input[contains(@id,'ReferenceNumberEditContainerBody')]").click()
-    #element.send_keys("123")
+
 
 finally:
 
