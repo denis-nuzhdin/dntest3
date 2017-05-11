23ec530ed9c1e49f054651bcf0819e7ed08377fd
denis-nuzhdin <kashmir@list.ru>
2017-05-11 22:19:04 +0400

    new field fill

--- test/open_page.py
+++ test/open_page.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 from selenium.webdriver.firefox.webdriver import WebDriver
-from selenium.webdriver.common.action_chains import ActionChains
+import datetime
 from selenium.webdriver.common.keys import Keys
+
+#from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.common.action_chains import ActionChains
 import time
 
 success = True
@@ -10,6 +13,9 @@ wd.implicitly_wait(60)
 main_page = "http://demo07.i-sys.ru:3003/"
 username = "i-sys\dt-tst-1"
 password = "PassW0rd"
+curent_date = datetime.datetime.now().strftime("%d.%m.%Y")
+
+
 
 def open_start_page():
     wd.get(main_page)
@@ -29,8 +35,32 @@ try:
     wd.switch_to.frame(frame)
 
     #Исходящий номер
-    ReferenceNumber = wd.find_element_by_xpath(".//input[contains(@id,'ReferenceNumberEditContainerBody')]")
+    ReferenceNumber = wd.find_element_by_xpath(".//input[contains(@id,'ReferenceNumber')]")
     ReferenceNumber.send_keys("123")
+
+    #Исходящая дата
+    ReferenceDate = wd.find_element_by_xpath(".//input[contains(@id,'ReferenceDate')]")
+    ReferenceDate.send_keys(curent_date)
+
+    #Организация отправителя
+    Organization = wd.find_element_by_xpath(".//td/div[contains(@id,'Organization')]")
+    Organization.send_keys('ОАО "Энергия"')
+    Organization.send_keys(Keys.RETURN)
+
+    #Краткое содержание
+    Summary = wd.find_element_by_xpath(".//textarea[contains(@id,'Summary')]")
+    Summary.send_keys("многострочный текст")
+
+    #Адресаты
+    Addressee = wd.find_element_by_xpath(".//td/div[contains(@id,'Addressee')]")
+    Addressee.send_keys("dt-tst-1")
+    Addressee.send_keys(Keys.RETURN)
+
+
+
+    #Срочно
+    Urgently = wd.find_element_by_xpath(".//input[contains(@id,'Urgently')]")
+    Urgently.click()
 
 
     #поиск инпута по названию поля
